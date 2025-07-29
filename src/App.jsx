99e54e3e0396a9dfe39a77ccf721149e91f1dea3
SaloniKhatri <122539374+SaloniKhatri@users.noreply.github.com>
2025-07-29 19:55:22 +0530

    Updated style

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/my-portfolio" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
