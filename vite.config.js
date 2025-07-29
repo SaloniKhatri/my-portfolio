bc5e9d826848d8b7817d029638b78852486875a0
SaloniKhatri <122539374+SaloniKhatri@users.noreply.github.com>
2025-07-29 19:37:43 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio', 
+  base: '/my-portfolio/', // Adjust the base path if your project is hosted in a subdirectory 
 })
